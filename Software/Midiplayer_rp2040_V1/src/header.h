cb908514c7470ea966e02ef72e3cd43e35498556
hm-gewiese <f.gewiese@gmx.de>
2025-05-29 23:49:06 +0200

    wirked on iS2

--- Software/Midiplayer_rp2040_V1/src/header.h
+++ Software/Midiplayer_rp2040_V1/src/header.h
@@ -28,13 +28,13 @@ typedef enum
     PIN_BUTTON_1 = 21,
     PIN_BUZZ = 22,
     PIN_HEADPHONE_CONNECT = 22,
-    PIN_AUDIO_0 = 23,
     PIN_SD_MOSI = 24,
     PIN_SD_CS = 25,
     PIN_SD_SCK = 26,
     PIN_SD_MISO = 27,
-    PIN_AUDIO_1 = 28,
-    PIN_AUDIO_2 = 29
+    PIN_AUDIO_LRCK = 23, //Audio_0
+    PIN_AUDIO_DIN = 28, //Audio_1
+    PIN_AUDIO_BCK = 29 //Audio_2
 } PIN_MAPPING;
 
 uint32_t scan_keys();
--- Software/Midiplayer_rp2040_V1/src/main.cpp
+++ Software/Midiplayer_rp2040_V1/src/main.cpp
@@ -1,30 +1,99 @@
-#include <Arduino.h>
-#include "header.h"
 #include "debug_in_color.h"
+#include "header.h"
+#include <Arduino.h>
+#include <I2S.h>
 
-void setup()
-{
+/**
+ * usefull commands:
+ * Compile, flash and monitor:
+ * ❯ pio run --target upload --target monitor
+ * Linux
+ * ❯ sudo ~/.platformio/penv/bin/pio run --target upload 
+ * clean
+ * ❯ pio run --target fullclean --environment phenobottle_firmware 
+ * Generate compile_commands.json (for clang-uml/neovim or simular):
+ * ❯ pio run -t compiledb
+ * List installed Libaries
+ * ❯ platformio lib list
+ * Install Libarie
+ * ❯ platformio lib install "ArduinoJson"
+ */
+
+void check_headphones();
+
+I2S i2s(OUTPUT, PIN_AUDIO_BCK, PIN_AUDIO_DIN);
+
+void setup() {
   Serial.begin(69);
   Serial.println("Start");
-  for (uint8_t i = PIN_FIRST_PAD; i < (PIN_FIRST_PAD + nPAD); i++)
-  {
-    pinMode(i, INPUT);
+  // for (uint8_t i = PIN_FIRST_PAD; i < (PIN_FIRST_PAD + nPAD); i++) {
+  //   pinMode(i, INPUT);
+  // }
+  // test Audio Jack
+  i2s.setBitsPerSample(32);
+  if (!i2s.begin(32000)) {
+    while (1) {
+      Serial.println("Failed to initialize I2S!");
+      delay(100);
+    }
   }
+  // test BUZZER
+  // pinMode(PIN_BUZZ, OUTPUT);
+  // analogWriteFreq(300);
+  // analogWriteRange(100);
+  // analogWrite(PIN_BUZZ, 50);
+  pinMode(PIN_BUZZ, INPUT_PULLUP);
 }
 
-void loop()
-{
-  debug("pressed keys:");
-  debugln(scan_keys(),BIN);
-  delay(100);
+const int frequency = 440; // frequency of square wave in Hz
+const int amplitude = 500; // amplitude of square wave
+const int sampleRate = 32000; // minimum for UDA1334A
+
+const int halfWavelength = (sampleRate / frequency); // half wavelength of square wave
+
+int16_t sample = amplitude; // current sample value
+int count = 0;
+void loop() {
+  // check_headphones();
+
+  if (count % halfWavelength == 0) {
+    sample = -1 * sample;
+  }
+  i2s.write(sample);
+  i2s.write(sample);
+  count++;
+  //
+  // static int i = 0;
+  // float angle = 2 * PI * 440.0 * i / 44100;
+  // int16_t sample = sin(angle) * 32767;
+  // i2s.write(sample); // Left
+  // i2s.write(sample); // Right
+  // i++;
+  // if (i >= 44100)
+  //   i = 0;
 }
 
-uint32_t scan_keys()
-{
+uint32_t scan_keys() {
   uint32_t buffer = 0;
-  for (uint8_t i = 0; i < 29; i++)
-  {
-    buffer | i<<digitalRead(i);
+  for (uint8_t i = 0; i < 29; i++) {
+    buffer | i << digitalRead(i);
   }
   return buffer;
+}
+
+void check_headphones() {
+  const int DELAY = 100;
+  static unsigned long timestamp = millis() + DELAY;
+  if (millis() > timestamp) {
+    timestamp = millis() + DELAY;
+
+    static int print_before = 0;
+    if (digitalRead(PIN_BUZZ) && !print_before) {
+      Serial.print("Headphone plugged\n");
+      print_before = 1;
+    } else if (!digitalRead(PIN_BUZZ) && print_before) {
+      Serial.print("Headphone unplugged\n");
+      print_before = 0;
+    }
+  }
 }
